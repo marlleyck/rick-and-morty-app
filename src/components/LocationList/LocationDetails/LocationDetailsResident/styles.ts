7e2f9dfc7264c1cafe5382f056eaeeb9a0451f08
marlleyck <marlleyck@hotmail.com>
2022-10-24 23:43:38 -0300

    style: location touchoppacity now

--- src/components/LocationList/LocationDetails/LocationDetailsResident/styles.ts
+++ src/components/LocationList/LocationDetails/LocationDetailsResident/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 import { THEME } from "../../../../THEME";
 
-export const Container = styled.View`
+export const Container = styled.TouchableOpacity`
     width: 100%;
     height: 80px;
 
